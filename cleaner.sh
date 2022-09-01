45ce3c919305062a44db09c0f02f85e77e8951f6
Riki Irawan <lightningthunder9999@gmail.com>
2022-09-01 16:50:48 +0000

    Upload New File

---
+++ cleaner.sh
@@ -0,0 +1,14 @@
+MODPATH=${0%/*}
+APP="`ls $MODPATH/system/priv-app` `ls $MODPATH/system/app`"
+for APPS in $APP; do
+  rm -f `find /data/system/package_cache -type f -name *$APPS*`
+  rm -f `find /data/dalvik-cache /data/resource-cache -type f -name *$APPS*.apk`
+done
+PKG="com.motorola.motosignature.app
+     com.motorola.dolby.dolbyui
+     com.dolby.daxservice"
+for PKGS in $PKG; do
+  rm -rf /data/user/*/$PKGS/cache/*
+done
+
+

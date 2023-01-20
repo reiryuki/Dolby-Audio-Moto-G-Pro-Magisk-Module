fc9d279c6da0b1f2edd9364ac22533c72e2ac2e4
Riki Irawan <lightningthunder9999@gmail.com>
2023-01-20 11:28:46 +0000

    Replace cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -4,9 +4,7 @@ for APPS in $APP; do
   rm -f `find /data/system/package_cache -type f -name *$APPS*`
   rm -f `find /data/dalvik-cache /data/resource-cache -type f -name *$APPS*.apk`
 done
-PKG="com.motorola.motosignature.app
-     com.motorola.dolby.dolbyui
-     com.dolby.daxservice"
+PKG=`cat $MODPATH/package.txt`
 for PKGS in $PKG; do
   rm -rf /data/user/*/$PKGS/cache/*
 done
